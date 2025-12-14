148f5df5cd4039ee43c5130c059bf908f7a3b7ab
manoda98 <manodasene@gmail.com>
2025-12-14 14:18:06 +0530

    Add lesson1 C++ files

---
+++ lesson1/helloworld.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+int main(){
+
+    //This is a comment
+    /*
+      this is a 
+      multiline comment 
+    */
+    std::cout << "I like pizza!" << '\n';
+    std::cout << "It's really good!" << '\n';
+    return 0;
+}
