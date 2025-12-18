4cfa23c26eb697f7064ba00a0e546f90d9d64a62
manoda98 <manodasene@gmail.com>
2025-12-18 20:18:09 +0530

    add hypotenuse calculation example

---
+++ sample-projects/hypotenuse.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+
+int main(){
+    using namespace std;
+    double a;
+    double b;
+    double c;
+
+    cout << "Enter side a: ";
+    cin >> a;
+    cout << "Enter side b: ";
+    cin >> b;
+
+    c = sqrt(pow(a,2) + pow(b,2));
+
+    cout << "side c: " << c;
+
+
+
+
+
+    return 0;
+}
