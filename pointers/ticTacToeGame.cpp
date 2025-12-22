5b598c3b9c846f1cab7c6d081555475fa432eb6c
manoda98 <manodasene@gmail.com>
2025-12-22 20:07:19 +0530

    Implement computerMove and alternate turns

--- pointers/ticTacToeGame.cpp
+++ pointers/ticTacToeGame.cpp
@@ -19,6 +19,9 @@ int main(){
     while(running){
         playerMove(spaces, player);
         drawBoard(spaces);
+
+        computerMove(spaces, computer);
+        drawBoard(spaces);
     }
     return 0;
 }
@@ -51,7 +54,16 @@ void playerMove(char *spaces, char player){
 
 }
 void computerMove(char *spaces, char computer){
+    int number;
+    srand(time(0));
 
+    while(true){
+        number = rand() % 9;
+        if(spaces[number] == ' '){
+            spaces[number] = computer;
+            break;
+        }
+    }
 }
 bool checkWinner(char *spaces, char player, char computer){
     return 0;
